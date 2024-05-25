e391ad06c3ad1884c7d77874a2b1238fb55dd8fa
tetedacier <fabien.jacq@gmail.com>
2024-05-25 10:23:12 +0200

    Add basic usage synopsis

--- mod.ts
+++ mod.ts
@@ -1,1 +1,32 @@
-// empty exports
+/**
+ * @module
+ *
+ * This module contains functions to search the database.
+ *
+ * @example
+ * ```ts
+ * import init from "@proteiform/bash";
+ *
+ * // initialize a shell
+ * const shell = init();
+ *
+ * // create a pre tag at the bottom of the first `main` element
+ * const shellPlaceholder = document.createElement('pre');
+ * const shellContainer = document.getElementTagName('main')[0];
+ *
+ * if (shellContainer === null) {
+ *     throw new Error('no main tag has been found in the document');
+ * }
+ *
+ * shellContainer.appendChild(shellPlaceholder);
+ *
+ * // attach initialized shell to it
+ * shell.attach(shellPlaceHolder);
+ * ```
+ */
+
+export default function init() {
+    return ({
+        attach: () => { }
+    });
+}
